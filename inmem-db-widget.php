99111280a1ffaa37f8f0cdd581076f5748dd39df
pjsinco <psinco@gmail.com>
2014-11-05 11:00:49 -0600

    changes action attr in 3 form elements

--- inmem-db-widget.php
+++ inmem-db-widget.php
@@ -2,7 +2,7 @@
 <p>
 <p>Search for the names of osteopathic physicians and osteopathic medical students who have died in the past two years.</p>
 <p><strong>Search by school</strong></p>
-<form name="school_choice" action="./inmem-db3.php" method="get">
+<form name="school_choice" action="<?php echo get_bloginfo('wpurl') . '/inmem-db3.php'; ?>" method="get">
 	<p><select name="school"> 
         <?php $collection->printDropdownBySchool() ?>
 	</select></p>
@@ -11,7 +11,7 @@
 </form>
 
 <p><strong>Search all schools by graduation year</strong></p>
-<form name="year_choice" action="./inmem-db3.php" method="get">
+<form name="year_choice" action="<?php echo get_bloginfo('wpurl') . '/inmem-db3.php'; ?>" method="get">
 	<select name="year">
 		<?php $collection->printDropdownByYear(); ?>
 	</select>
@@ -20,7 +20,7 @@
 </form>
 
 <p><strong>Search by state of residence</strong></p>
-<form name="state_choice" action="./inmem-db3.php" method="get">
+<form name="state_choice" action="<?php echo get_bloginfo('wpurl') . '/inmem-db3.php'; ?>" method="get">
 	<select name="state">
 		<?php $collection->printDropdownByState(); ?>
 	</select>
